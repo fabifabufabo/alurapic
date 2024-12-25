a0e942d25ebb1245ee21a80ef9919692960a081e
Fábio Fernandes <dev.fabiofernandes@gmail.com>
2024-12-25 05:24:50 -0300

    feat: implement registration form

--- src/components/register/Register.vue
+++ src/components/register/Register.vue
@@ -1,10 +1,85 @@
 <template>
-  <h2 class="centralized">Cadastro</h2>
+  <div>
+    <h1 class="centralized">Cadastro</h1>
+    <h2 class="centralized">{{ picture.titulo }}</h2>
+
+    <form @submit.prevent="record()">
+      <div class="control">
+        <label for="titulo">TÍTULO</label>
+        <input id="titulo" autocomplete="off" v-model="picture.titulo" />
+      </div>
+
+      <div class="control">
+        <label for="url">URL</label>
+        <input id="url" autocomplete="off" v-model.lazy="picture.url" />
+        <responsive-image
+          v-show="picture.url"
+          :url="picture.url"
+          :titulo="picture.titulo"
+        />
+      </div>
+
+      <div class="control">
+        <label for="descricao">DESCRIÇÃO</label>
+        <textarea id="descricao" autocomplete="off" v-model="picture.descricao">
+        </textarea>
+      </div>
+
+      <div class="centralized">
+        <my-button buttonlabel="GRAVAR" buttontype="submit" />
+        <router-link to="/">
+          <my-button buttonlabel="VOLTAR" buttontype="button" />
+        </router-link>
+      </div>
+    </form>
+  </div>
 </template>
 
-<script></script>
+<script>
+import ResponsiveImage from '../shared/responsive-image/ResponsiveImage.vue';
+import Button from '../shared/button/Button.vue';
+import Picture from '../../domain/picture/Picture';
 
-<style>
+export default {
+  components: {
+    'responsive-image': ResponsiveImage,
+    'my-button': Button,
+  },
+  data() {
+    return {
+      picture: new Picture(),
+    };
+  },
+  methods: {
+    record() {
+      this.$http.post('http://localhost:3000/v1/fotos', this.picture).then(
+        () => (this.picture = new Picture()),
+        (err) => console.log(err)
+      );
+    },
+  },
+};
+</script>
+<style scoped>
+.centralized {
+  text-align: center;
+}
+.control {
+  font-size: 1.2em;
+  margin-bottom: 20px;
+}
+.control label {
+  display: block;
+  font-weight: bold;
+}
+
+.control label + input,
+.control textarea {
+  width: 100%;
+  font-size: inherit;
+  border-radius: 5px;
+}
+
 .centralized {
   text-align: center;
 }
